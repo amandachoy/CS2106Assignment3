92e8ed74696f8bcd8020c833b93c6b13e76182aa
liuyang <melody9951213@gmail.com>
2017-04-11 04:48:28 +0800

    draft for the remaining part

--- SEM2-AY1617/CS2106/Assignments/Assignment3/Source/libefs.cpp
+++ SEM2-AY1617/CS2106/Assignments/Assignment3/Source/libefs.cpp
@@ -17,9 +17,10 @@ void initFS(const char *fsPartitionName, const char *fsPassword)
     mountFS(fsPartitionName, fsPassword);
     _fs = getFSInfo();
     _oft = (TOpenFile *) calloc(sizeof(TOpenFile), _fs->maxFiles);
+    _oftCount = 0;
 }
 
-int createOpenFileEntry(int mode, unsigned inode, unsigned long len) {
+int createOpenFileEntry(int mode, unsigned int inode, unsigned long len) {
 	_oft[_oftCount].openMode = mode;
 	_oft[_oftCount].blockSize = _fs->blockSize;
 	_oft[_oftCount].inode = inode;
@@ -27,7 +28,7 @@ int createOpenFileEntry(int mode, unsigned inode, unsigned long len) {
 	loadInode(inodeBuffer, inode);
 	_oft[_oftCount].inodeBuffer = inodeBuffer;
 	_oft[_oftCount].buffer = makeDataBuffer();
-	_oft[_oftCount].writePtr = 0;
+	_oft[_oftCount].writePtr = (mode == MODE_READ_APPEND ? (len % _fs->blockSize) : 0);
 	_oft[_oftCount].readPtr = 0;
 	_oft[_oftCount].filePtr = (mode == MODE_READ_APPEND ? len : 0);
 	_oftCount++;
@@ -82,87 +83,109 @@ int openFile(const char *filename, unsigned char mode)
 // if file is opened in MODE_READ_ONLY mode.
 void writeFile(int fp, void *buffer, unsigned int dataSize, unsigned int dataCount)
 {
-    if (_oft[fp].openMode == MODE_READ_ONLY) {
+	TOpenFile f = _oft[fp];
+    if (f.openMode == MODE_READ_ONLY || dataSize <= 0) {
         return;
     }
-    unsigned long* inodeBuffer = makeInodeBuffer();
-    loadInode(inodeBuffer, fp);
-    
-    int i = 0;
-
-    unsigned long blockNum = inodeBuffer[i];
-    unsigned int totalData = dataSize * dataCount;
-    int j = 0;
-    
-    while (j < ceil(totalData/(_fs->blockSize))) {
-        int currentOffset = j * _fs->blockSize;
-        writeBlock((char*)buffer + currentOffset, blockNum);
-        unsigned long nextBlock = findFreeBlock();
-        markBlockBusy(nextBlock);
-        inodeBuffer[i++] = nextBlock;
-        updateFreeList();
-        j++;
-    }
-    
-    saveInode(inodeBuffer, fp);
-    updateDirectory();
-    free(inodeBuffer);
-    free(buffer);
+  
+    unsigned int remaining = dataSize, lenToWriteIntoThisBlock;
+    unsigned long blockNumber;
+	
+	while(remaining > 0) {
+		blockNumber = returnBlockNumFromInode(f.inodeBuffer, f.filePtr);
+		if(blockNumber == 0){
+			blockNumber = findFreeBlock();
+			markBlockBusy(blockNumber);
+			setBlockNumInInode(f.inodeBuffer, f.filePtr, blockNumber);
+		}
+		lenToWriteIntoThisBlock = f.blockSize - f.writePtr < remaining ?
+								  (f.blockSize - f.writePtr) : remaining;
+		strncpy(f.buffer, (char *)buffer+(dataSize-remaining), lenToWriteIntoThisBlock);
+		f.filePtr = f.filePtr + lenToWriteIntoThisBlock;
+		f.writePtr = (f.writePtr + lenToWriteIntoThisBlock) % f.blockSize;
+		remaining -= lenToWriteIntoThisBlock;
+		if(f.writePtr == 0) {
+			writeBlock(f.buffer, blockNumber);
+		}
+	}
+	//~ updateDirectoryFileLength(f.filePtr);
 }
 
 // Flush the file data to the disk. Writes all data buffers, updates directory,
 // free list and inode for this file.
-//??
+
 void flushFile(int fp)
 {
-    updateFreeList();
+	TOpenFile f = _oft[fp];
+    if (f.openMode == MODE_READ_ONLY) {
+        return;
+    }
+	if(f.writePtr != 0) {
+		memset(f.buffer + f.writePtr, 0, f.blockSize - f.writePtr);
+		writeBlock(f.buffer, returnBlockNumFromInode(f.inodeBuffer, f.filePtr));
+	}    
+	saveInode(f.inodeBuffer, f.inode);
+	updateFreeList();
     updateDirectory();
 }
 
 // Read data from the file.
 void readFile(int fp, void *buffer, unsigned int dataSize, unsigned int dataCount)
 {
-    unsigned long* inodeBuffer = makeInodeBuffer();
-    loadInode(inodeBuffer, fp);
-    
-    int i = 0;
-    
-    unsigned long blockNum = inodeBuffer[i];
-    unsigned int totalData = dataSize * dataCount;
-    int j = 0;
-    
-    while (j < ceil(totalData/(_fs->blockSize))) {
-        int currentOffset = j * _fs->blockSize;
-        readBlock((char*)buffer + currentOffset, blockNum);
-        blockNum = inodeBuffer[i++];
-        j++;
+	TOpenFile f = _oft[fp];
+    if (dataSize <= 0) {
+        return;
     }
-    
-    saveInode(inodeBuffer, fp);
-    free(inodeBuffer);
+  
+    unsigned int remaining = dataSize, lenToReadFromThisBlock;
+    unsigned long blockNumber;
+	
+	while(remaining > 0) {
+		blockNumber = returnBlockNumFromInode(f.inodeBuffer, f.filePtr);
+		if(blockNumber == 0){
+			memset((char *)buffer + (dataSize - remaining), 0, f.blockSize);
+		}
+		lenToReadFromThisBlock = f.blockSize - f.readPtr < remaining ?
+								  (f.blockSize - f.readPtr) : remaining;
+		strncpy((char *)buffer+(dataSize-remaining), f.buffer, lenToReadFromThisBlock);
+		f.filePtr = f.filePtr + lenToReadFromThisBlock;
+		f.readPtr = (f.readPtr + lenToReadFromThisBlock) % f.blockSize;
+		remaining -= lenToReadFromThisBlock;
+	}
 }
 
 // Delete the file. Read-only flag (bit 2 of the attr field) in directory listing must not be set. 
 // See TDirectory structure.
 void delFile(const char *filename) {
-    unsigned int attr = getAttr(filename);
-    unsigned int index = findFile(filename);
-    if (_result != FS_FILE_NOT_FOUND) {
-        unsigned long *inode;
-        loadInode(inode, index);
-        if (attr >> 2 == 0) {
-            markBlockFree(inode[0]);
-            delDirectoryEntry(filename);
-        } else {
-            printf("This file is READ ONLY");
-        }
-        updateFreeList();
-        updateDirectory();
-    }
+    unsigned int i = findFile(filename);
+    if (_result == FS_OK) {
+		unsigned int attr = getAttr(filename);
+		bool readonly = attr | (1<<6);
+		if(readonly) {
+			return;
+		} else {
+			int numOfBlocks = getFileLength(filename) / _fs->blockSize;
+			unsigned long *inodeBuffer = makeInodeBuffer();
+			loadInode(inodeBuffer, i);
+			for(int i=0;i<numOfBlocks;i++){
+				if(inodeBuffer[i] != 0) {
+					markBlockFree(inodeBuffer[i]);
+					inodeBuffer[i] = 0;
+				}
+			}
+			updateFreeList();
+			saveInode(inodeBuffer, i);
+			delDirectoryEntry(filename);
+			updateDirectory();
+		}
+	}
+	
+	return;
 }
 
 // Close a file. Flushes all data buffers, updates inode, directory, etc.
 void closeFile(int fp) {
+	flushFile(fp);
     free(_oft[fp].buffer);
     free(_oft[fp].inodeBuffer);
 }
@@ -170,6 +193,15 @@ void closeFile(int fp) {
 
 // Unmount file system.
 void closeFS() {
+	for(int i = 0; i < _oftCount; i++){
+		closeFile(i);
+	}
     unmountFS();
+    free(_fs);
+    free(_oft);
+}
+
+int main() {
+	return 0;
 }
 
