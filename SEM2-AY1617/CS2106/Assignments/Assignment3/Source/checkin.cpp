8b78f89bc7d0733f158bbe810c5acc07e9889155
liuyang <melody9951213@gmail.com>
2017-04-14 06:27:45 +0800

    check in and check out

--- SEM2-AY1617/CS2106/Assignments/Assignment3/Source/checkin.cpp
+++ SEM2-AY1617/CS2106/Assignments/Assignment3/Source/checkin.cpp
@@ -9,13 +9,44 @@ int main(int ac, char **av)
 	}
     
     initFS("part.dsk", av[2]);
-    unsigned int index = findFile(av[1]);
+    if (_result == FS_ERROR) {
+		printf("Unknown Error\n");
+		exit(-1);
+	}
+	
+    findFile(av[1]);
 
-    if (_result == FS_OK) {
+	if (_result == FS_FILE_NOT_FOUND) {
+		unsigned int fileInEFS = openFile(av[1], MODE_CREATE);
+		
+		FILE *fp = fopen(av[1], "r");
+		if(fp == NULL)
+		{
+			printf("\nUnable to open source file %s\n\n", av[1]);
+			exit(-1);
+		}
+        
+		// get the size of the file
+        fseek(fp, 0, SEEK_END);
+        unsigned long size = ftell(fp);
+        fseek(fp, 0, SEEK_SET);
+        
+		void *buffer = malloc(size);		
+		fread(buffer, sizeof(char), size, fp);
+		fclose(fp);
+		
+		writeFile(fileInEFS, (void *) buffer, sizeof(char), size);
+		flushFile(fileInEFS);
+		closeFile(fileInEFS);
+		
+		unmountFS();
+		free(buffer);
+    } else if (_result == FS_OK){
         printf("DUPLICATE FILE\n");
-    } else if (_result == FS_FILE_NOT_FOUND) {
-        openFile(av[1], 1);
-        printf("Checked In\n");
-    }
+        exit(-1);
+    } else {
+		printf("Unknown Error\n");
+		exit(-1);
+	}
 	return 0;
 }
--- SEM2-AY1617/CS2106/Assignments/Assignment3/Source/checkout.cpp
+++ SEM2-AY1617/CS2106/Assignments/Assignment3/Source/checkout.cpp
@@ -8,16 +8,41 @@ int main(int ac, char **av)
 		return -1;
 	}
     
-    initFS("part.dsk", av[2]);
-    unsigned int index = findFile(av[1]);
-    if (_result == FS_FILE_NOT_FOUND) {
+    initFS("part.dsk", av[2]);    
+    if (_result == FS_ERROR) {
+		printf("Unknown Error\n");
+		exit(-1);
+	}
+	
+	findFile(av[1]);
+
+	if (_result == FS_OK) {
+		unsigned int fileInEFS = openFile(av[1], MODE_READ_ONLY);
+		
+		FILE *fp = fopen(av[1], "w");
+		if(fp == NULL)
+		{
+			printf("\nUnable to open source file %s\n\n", av[1]);
+			exit(-1);
+		}
+        
+		unsigned long size = getFileLength(av[1]);
+		void *buffer = malloc(size);	
+		
+		readFile(fileInEFS, (void *) buffer, sizeof(char), size);
+		closeFile(fileInEFS);
+
+		fwrite(buffer, sizeof(char), size, fp);	
+		fclose(fp);
+
+		unmountFS();
+		free(buffer);
+    } else if (_result == FS_FILE_NOT_FOUND){
         printf("FILE NOT FOUND\n");
+        exit(-1);
     } else {
-        flushFile(index);
-        closeFile(index);
-        closeFS();
-        printf("Checked Out\n");
-    }
-    
+		printf("Unknown Error\n");
+		exit(-1);
+	}
 	return 0;
 }
